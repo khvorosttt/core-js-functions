c06b19f04595cff49c307a381d5925b3c71d669b
khvorosttt <khvorosttt@yandex.by>
2024-03-21 19:48:59 +0300

    feat: add implementation getFunctionBody

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -32,8 +32,11 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (!arguments.length) {
+    return '';
+  }
+  return func.toString();
 }
 
 /**
